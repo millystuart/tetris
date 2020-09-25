d355f52908b0f24844eff000ef6ce9589e877ac1
Melissa Stuart <melissaaa.stuart@gmail.com>
2020-09-25 17:52:30 +0100

    Organise and remove redundant code

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 import pygame
-import requests
-import io
 
 pygame.init()
 
-display_width = 800 # Width of initial window
-display_height = 600 # Length of initial window
+def display():
 
-game_display = pygame.display.set_mode((display_width,display_height)) 
-pygame.display.set_caption("Tetris") # Name of window
+    width = 600 # Width of initial window
+    height = 400 # Length of initial window
 
-black = (0,0,0) # Here, just initialising some colours for my game
-white = (255,255,255)
+    black = (0,0,0) 
+    white = (255,255,255)
+    turquoise = (70,255,255)# Initialising tetrimino colours
+    blue = (0,60,255)
+    orange = (255,200,0)
+    yellow = (255,250,0)
+    green = (0,250,0)
+    purple = (185,0,255)
+    red = (255,0,0)
 
-clock = pygame.time.Clock() # Game clock to track time within the game
-crashed = False # Game hasn't been exited yet!
+    game_display = pygame.display.set_mode((width,height)) 
+    frame_rate = 30
+    pygame.display.set_caption("Tetris") # Name of window
+    game_display.fill(turquoise) # To change the background colour
 
-img_url = "https://vignette.wikia.nocookie.net/logopedia/images/9/99/Tetris-3d.png/revision/latest?cb=20190302020005"
+    pygame.display.flip()
 
-r = requests.get(img_url)
-img_data = io.BytesIO(r.content)
+    is_running = True # Game hasn't been exited yet!
+    while is_running == True: # While loop keeping the window from closing unless is_running == False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+                pygame.quit()
 
-tetris_img = pygame.image.load(img_data) # Image that I'll be displaying on my window
-
-def logo(x_img,y_img):
-    game_display.blit(tetris_img,(x_img,y_img)) # "blit" function gets the image onto the window
-    
-x_img = 0.5 # Image size
-y_img = 0.2
-
-while crashed == False: # While loop keeping the 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: # If the user attempts to exit the window, program will crash
-            crashed = True
-        print(event)
-
-    game_display.fill(white) # To change the background colour
-    logo(x_img,y_img)
-
-    pygame.display.update()
-    clock.tick(30) # How many frames per second (fps) are being run
+display()
