6070828f67e16ef882aa732e318c319b84accc40
Melissa Stuart <melissaaa.stuart@gmail.com>
2020-10-12 20:00:25 +0100

    Add initial test for coords_tetrimino

--- test_tetris.py
+++ test_tetris.py
@@ -1,5 +1,9 @@
 from tetris import *
 
 def test_coords_tetrimino():
-    assert coords_tetrimino(tetriminos[3], tetrimino_colours[3], 0, 0, 0) == []
+    assert coords_tetrimino(tetriminos[4], tetrimino_colour[4], 2, 0, 0) == [(0, 1, blue), (1, 1, blue), (2, 1, blue), (2, 2, blue)]
+
+""" [[0, 0, 0]
+     [1, 1, 1]
+     [0, 0, 1]] """
     
