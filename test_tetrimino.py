9b86a230bee12094bf115bfc499fecfc4a63a9d9
Melissa Stuart <melissaaa.stuart@gmail.com>
2020-10-12 20:14:05 +0100

    Re-name files for clarity

--- test_tetrimino.py
+++ test_tetrimino.py
@@ -1,4 +1,4 @@
-from tetris import *
+from tetrimino import *
 
 def test_coords_tetrimino():
     assert coords_tetrimino(tetriminos[4], tetrimino_colour[4], 2, 0, 0) == [(0, 1, blue), (1, 1, blue), (2, 1, blue), (2, 2, blue)]
