4114b77647da439b1e82cb79f19a164dc4d79800
Melissa Stuart <melissaaa.stuart@gmail.com>
2020-10-16 12:01:05 +0100

    Implement tetrimino dicts with tests passing

--- test_tetrimino.py
+++ test_tetrimino.py
@@ -1,7 +1,7 @@
 from tetrimino import *
 
 def test_coords_tetrimino(): # will compare the value of coords_tetrimino() with my expected output below
-    assert coords_tetrimino(4, tetrimino_colour[4], 2, 3, 4) == [(3, 5, blue), (4, 5, blue), (5, 5, blue), (5, 6, blue)]
+    assert coords_tetrimino(4, J["colour"], 2, 3, 4) == [(3, 5, blue), (4, 5, blue), (5, 5, blue), (5, 6, blue)]
 
 """ [[0, 0, 0]
      [1, 1, 1]
